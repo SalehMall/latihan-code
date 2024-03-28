66f2d8d0e7b048973591b215eb45111c805eb10c
SalehMall <kemalbadawi@gmail.com>
2024-03-28 09:39:15 +0800

    tambah folder

---
+++ pratek 27/02/2024 (javascript)/script.js
@@ -0,0 +1,22 @@
+let message
+message = "hello!"
+
+alert(message);
+
+let message2 = "hello world";
+alert(message2);
+
+let user = "smith";
+let age = "19";
+let message3 = "hello smith";
+
+
+let user2 = 'john dom', age2 = '25', massage4 = 'hallo smith';
+
+//old script
+var massage5 = "hello var";
+
+const my_birthday ="28 mei 2024";
+my_birthday ="20 mei 2024";
+
+console.log(my_birthday);
